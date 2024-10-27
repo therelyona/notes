fef23d45afb50e8cf4ed8e9040da177d7b24b5b9
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-27 20:56:22 +0400

    modified eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import globals from 'globals';
-
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
@@ -20,15 +20,16 @@ export default [
       globals: {
         ...globals.node,
         ...globals.jest,
+        ...globals.browser,
       },
       parserOptions: {
-        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
-        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
     },
-    plugins: { import: importPlugin },
+    plugins: {
+      import: importPlugin,
+    },
     rules: {
       ...importPlugin.configs.recommended.rules,
     },
