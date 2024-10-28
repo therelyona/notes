d63bd838867936c50b086de2a5f14df6b180b5a9
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-28 16:38:35 +0400

    fix app.js

--- src/app.js
+++ src/app.js
@@ -11,28 +11,16 @@ const elements = {
   searchInput: document.getElementById('searchInput'),
 };
 
-const deleteNote = (index) => {
-  notes.splice(index, 1);
-  localStorage.setItem('notes', JSON.stringify(notes));
-  updateDisplay();
-};
-
 const editNote = (index) => {
   elements.noteText.value = notes[index].text;
   elements.noteTags.value = notes[index].tags;
   editIndex = index;
 };
 
-const updateDisplay = () => {
-  displayNotes(notes, elements);
-  notes.forEach((_, index) => {
-    const noteElements = elements.notesList.children[index];
-    const editBtn = noteElements.querySelector('.edit-btn');
-    const deleteBtn = noteElements.querySelector('.delete-btn');
-
-    editBtn.addEventListener('click', () => editNote(index));
-    deleteBtn.addEventListener('click', () => deleteNote(index));
-  });
+const deleteNote = (index) => {
+  notes.splice(index, 1);
+  localStorage.setItem('notes', JSON.stringify(notes));
+  displayNotes(notes, elements, editNote, deleteNote);
 };
 
 elements.saveNote.addEventListener('click', () => {
@@ -48,7 +36,7 @@ elements.saveNote.addEventListener('click', () => {
   }
 
   localStorage.setItem('notes', JSON.stringify(notes));
-  updateDisplay();
+  displayNotes(notes, elements, editNote, deleteNote);
   elements.noteText.value = '';
   elements.noteTags.value = '';
   editIndex = null;
@@ -59,7 +47,7 @@ elements.searchInput.addEventListener('input', () => {
   const filteredNotes = notes.filter((note) => note.text.toLowerCase().includes(searchTerm)
     || note.tags.toLowerCase().includes(searchTerm));
 
-  displayNotes(filteredNotes, elements);
+  displayNotes(filteredNotes, elements, editNote, deleteNote);
 });
 
-updateDisplay();
+displayNotes(notes, elements, editNote, deleteNote);
